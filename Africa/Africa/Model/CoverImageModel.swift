9d680e0b939fec12171f82edf083f9d9407a5f29
pick <picknavin556@gmail.com>
2022-09-26 23:47:21 +0700

    Add JSONDecoder

---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Supapon Pucknavin on 26/9/2565 BE.
+//
+
+import SwiftUI
+
+struct CoverImage : Codable, Identifiable {
+    let id: Int
+    let name: String
+}
